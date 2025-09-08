5e72a32fc68da7a35382b94bba3013c85684fac4
Israel Cadorin <cadorinisrael@gmail.com>
2025-09-07 23:54:28 -0300

    feat: create Transaction entity

---
+++ simplifiedpicpay/src/main/java/com/simplifiedpicpay/domain/transaction/Transaction.java
@@ -0,0 +1,95 @@
+package com.simplifiedpicpay.domain.transaction;
+
+import com.simplifiedpicpay.domain.user.User;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+
+    public Transaction(){
+    }
+
+    public Transaction(BigDecimal amount, User sender, User receiver) {
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp){
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
