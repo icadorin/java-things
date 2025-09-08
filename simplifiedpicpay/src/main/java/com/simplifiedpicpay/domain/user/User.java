791a353f40646691bfc3f8c19008a3592aaeed27
Israel Cadorin <cadorinisrael@gmail.com>
2025-09-07 23:50:41 -0300

    feat: first commit

---
+++ simplifiedpicpay/src/main/java/com/simplifiedpicpay/domain/user/User.java
@@ -0,0 +1,139 @@
+package com.simplifiedpicpay.domain.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity(name="users")
+@Table(name="users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique=true)
+    private String document;
+
+    @Column(unique=true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String document,
+                String email, String password, BigDecimal balance, UserType userType) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.userType = userType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public String getPassword () {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance () {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public UserType getUserType () {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", document='" + document + '\'' +
+                ", email='" + email + '\'' +
+                ", balance=" + balance +
+                ", userType=" + userType +
+                '}';
+    }
+}
